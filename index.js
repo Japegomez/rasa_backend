0c142f19891fdc307974375ed8828d8d6e002bf7
Javi Peña <japenago@gmail.com>
2023-01-31 14:25:20 +0100

    add post catalogo and bodyparser

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const express = require('express')
 var cors = require('cors')
+const body_parser = require('body-parser')
 const app = express()
 const port = 4200
 
+app.use(body_parser.urlencoded({ extended: true }))
 app.use(
 	cors({
 		origin: '*',
 	})
 )
 var catalogo = require('./catalogo.json')
-app.get('/', function (req, res) {
-	res.json(catalogo)
-	// comentario prueba
+
+app.post('/catalogo', function (req, res) {
+	console.log(req)
+	res.send(catalogo)
 })
 
 app.get('/palabrasclave', function (req, res) {
